dc9991cfd2e3c31592dfe21ba7b51d16e2245523
purushothaman-source <purushoth99jun@gmail.com>
2021-01-24 20:48:01 +0530

    ADDED : TC-1.1-GivenMaxNumberAt1stPosition

--- src/test/java/com/uccases/MaxFinderTest.java
+++ src/test/java/com/uccases/MaxFinderTest.java
@@ -5,5 +5,10 @@ import org.junit.Test;
 
 
 public class MaxFinderTest {
-
+    @Test
+    public void givenThreeInteger_ShouldReturnMaxvalue() {
+        MaxFinder maxFinder= new MaxFinder();
+        int max = maxFinder.findMax(400, 300, -150);
+        Assert.assertEquals(400,max);
+    }
 }

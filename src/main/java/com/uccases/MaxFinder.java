2f802eaf40b699f48239cc66e05ad162fdc305f1
purushothaman-source <purushoth99jun@gmail.com>
2021-01-24 22:32:45 +0530

    REFRACTORED  : refracted  to generic  class

--- src/main/java/com/uccases/MaxFinder.java
+++ src/main/java/com/uccases/MaxFinder.java
@@ -1,9 +1,19 @@
 package com.uccases;
 
-public class MaxFinder {
+public class MaxFinder<E extends  Comparable<E>> {
+          private E a,b,c;
 
-    public <E extends Comparable> E findMaxValue(E a, E b, E c) {
-        E max = a;
+    public MaxFinder(E a, E b, E c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    public E maximum(){
+        return findMaxValue(a,b,c);
+    }
+
+    public static <E extends Comparable> E findMaxValue(E a,E b,E c) {
+        E max =  a;
         if (b.compareTo(max) > 0 && b.compareTo(c) > 0)
             max = b;
         else if (c.compareTo(max) > 0)
--- src/test/java/com/uccases/MaxFinderTest.java
+++ src/test/java/com/uccases/MaxFinderTest.java
@@ -7,22 +7,22 @@ import org.junit.Test;
 public class MaxFinderTest {
     @Test
     public void givenThreeInteger_ShouldReturnMaxvalue() {
-        MaxFinder maxFinder = new MaxFinder();
-        int max = maxFinder.findMaxValue(300, -150, 400);
+        MaxFinder maxFinder = new MaxFinder(300, -150, 400);
+        int max = (int) maxFinder.maximum();
         Assert.assertEquals(400, max);
     }
 
     @Test
     public void givenThreeFloatValue_ShouldReturnMaxvalue() {
-        MaxFinder maxFinder = new MaxFinder();
-        Float max = maxFinder.findMaxValue(-150.0f, 300.0f, 400.00f);
-        Assert.assertEquals(400.0, max, 0);
+        MaxFinder maxFinder = new MaxFinder(-150.0f, 300.0f, 400.00f);
+        Float max= (Float) maxFinder.maximum();
+         Assert.assertEquals(400.0, max, 0);
     }
 
     @Test
     public void givenThreeStringValue_ShouldReturnMaxvalue() {
-        MaxFinder maxFinder = new MaxFinder();
-        String max = maxFinder.findMaxValue("Apple", "Banana", "Peach");
+        MaxFinder maxFinder = new MaxFinder("Apple", "Banana", "Peach");
+        String max = (String) maxFinder.maximum();
         Assert.assertEquals("Peach", max);
     }
 }

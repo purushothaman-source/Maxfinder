b201b47ad1458fa165283361e0ff8ca0e1952f94
purushothaman-source <purushoth99jun@gmail.com>
2021-01-24 21:32:10 +0530

    ADDED : TC-2.1-functionality to test max value at 1st position

--- src/main/java/com/uccases/MaxFinder.java
+++ src/main/java/com/uccases/MaxFinder.java
@@ -9,4 +9,13 @@ public class MaxFinder {
             max = c;
         return max;
     }
+
+    public float findFloatMax(Float a, Float b, Float c) {
+        Float max = a;
+        if (b.compareTo(max) > 0 && b.compareTo(c) > 0 )
+            max = b;
+        else if (c.compareTo(max) > 0)
+            max = c;
+        return max;
+    }
 }
--- src/test/java/com/uccases/MaxFinderTest.java
+++ src/test/java/com/uccases/MaxFinderTest.java
@@ -11,4 +11,11 @@ public class MaxFinderTest {
         int max = maxFinder.findMax(300, -150, 400);
         Assert.assertEquals(400,max);
     }
+
+    @Test
+    public void givenThreeFloatValue_ShouldReturnMaxvalue() {
+        MaxFinder maxFinder= new MaxFinder();
+        Float max = maxFinder.findFloatMax(400.0f, -150.00f, 300.00f);
+        Assert.assertEquals(400.0,max,0);
+    }
 }
